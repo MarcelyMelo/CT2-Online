75d0a1ba3e2e260677f833f907006ce8777d4073
Marcely Melo <MarcelyjfMelo@gmail.com>
2023-08-24 17:36:53 -0300

    aula 16 - 24/08/2023

--- Godot2/player.gd
+++ Godot2/player.gd
@@ -1,10 +1,12 @@
 extends Area2D
+signal hit
 
 @export var velocidade = 400
 var tamanho_tela
 
 func _ready():
 	tamanho_tela = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -22,8 +24,20 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-		
-# Esse é o código que estamos fazendo agora
+
 	position += velocity * delta
 	position.x = clamp(position.x, 0, tamanho_tela.x)
 	position.y = clamp(position.y, 0, tamanho_tela.y)
+	
+# 	Esse é o código que estamos fazendo hoje
+
+# != é "diferente de"
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "Andar"
+		$AnimatedSprite2D.flip_v = false 
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite2D.animation = "Cima"
+		$AnimatedSprite2D.flip_h = false 
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
